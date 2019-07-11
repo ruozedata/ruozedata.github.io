f6bdbb36f39c96023eb0f219bef7175286f19887
ruozedata <1990218038@qq.com>
2019-07-11 17:25:45 +0800

    Site updated: 2019-07-11 17:25:31

--- assets/demo/gift/js/index.main.js
+++ assets/demo/gift/js/index.main.js
@@ -1,3 +1,3 @@
-// build time:Tue Jul 09 2019 13:48:30 GMT+0800 (GMT+08:00)
+// build time:Thu Jul 11 2019 17:22:07 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
--- assets/demo/mediator/mediator.js
+++ assets/demo/mediator/mediator.js
@@ -1,2 +1,2 @@
-// build time:Tue Jul 09 2019 13:48:30 GMT+0800 (GMT+08:00)
+// build time:Thu Jul 11 2019 17:22:07 GMT+0800 (GMT+08:00)
 (function(){var e,t,n;e=function(e){this.points=0;return this.name=e};e.prototype.play=function(){this.points++;return t.played()};n={element:document.getElementById("results"),update:function(e){var t,n,r,s;r="";for(n in e){s=e[n];if(e.hasOwnProperty(n)){r=r+("<span><strong>"+n+"</strong>:"+s+"</span>")}}this.element.innerHTML=r;t=e.Home-e.Guest;if(t>15){alert("Home Win!");return location.reload()}else if(t
